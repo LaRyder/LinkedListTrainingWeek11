b8cc4a8a2c3d8eec5298177a3e49750fcb24f415
Lauren Ryder <lauren.ryder@microsoft.com>
2021-09-28 16:47:44 -0400

    finally got addAtIndex to pass

---
+++ LinkedListTrainingWeek11/LinkedListsTraining/LinkedListTraining_tests/MyLinkedList_tests.cs
@@ -0,0 +1,91 @@
+﻿using LinkedListsTraining;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedListTraining_tests
+{
+    [TestClass]
+    public class MyLinkedList_tests
+    {
+        MyLinkedList myLinkedList = new MyLinkedList();
+
+        public static class AssertLinkedList
+        {
+            public static void NodesHaveEqualValue(ListNode expected, ListNode actual)
+            {
+                while (expected != null)
+                {
+                    Assert.AreEqual(expected.val, actual.val);
+                    expected = expected.next;
+                    actual = actual.next;
+                }
+            }
+        }
+        [TestMethod]
+        /** Add a node of value val before the first element of the linked list. 
+         * After the insertion, the new node will be the first node of the linked list. */
+        public void AddAtHead_OneNumber_CanReturnNumber()
+        {
+            var input = 2;
+            var expected = 2;
+            var sut = new MyLinkedList();
+
+            sut.AddAtHead(input);
+
+            var actual = sut.Get(0);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        /** Append a node of value val to the last element of the linked list. */
+        public void AddAtTail_LastElement_CanReturnNumber()
+        {
+            var input = 3;
+            var expected = 3;
+            var sut = new MyLinkedList();
+
+            sut.AddAtTail(input);
+
+            var actual = sut.Get(0);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        /** Add a node of value val before the index-th node in the linked list. 
+         * If index equals to the length of linked list, the node will be appended to the end of linked list. 
+         * If index is greater than the length, the node will not be inserted. */
+        public void AddAtIndex_IfValid_CanReturnNumber()
+        {
+            var index = 1;
+            var valueToAdd = 2;
+            var sut = new MyLinkedList();
+
+            var expected = valueToAdd;
+            var actual = sut.Get(index);
+
+            sut.AddAtIndex(index, valueToAdd);
+
+            Assert.AreNotEqual(expected, actual);
+        }
+
+        [TestMethod]
+        /** Get the value of the index-th node in the linked list. If the index is invalid, return -1. */
+        public void Get_Value_CanReturnANumber()
+        {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod]
+        /** Delete the index-th node in the linked list, if the index is valid. */
+        public void DeleteAtIndex_IfValid_CanReturnNumber()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ LinkedListTrainingWeek11/LinkedListsTraining/LinkedListsTraining/MyLinkedList.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedListsTraining
+{
+    public class MyLinkedList
+    {
+        public class Program
+        {
+            /*Input
+            ["MyLinkedList", "addAtHead", "addAtTail", "addAtIndex", "get", "deleteAtIndex", "get"]
+            [[], [1], [3], [1, 2], [1], [1], [1]]
+            Output
+            [null, null, null, null, 2, null, 3]*/
+            public static void LinkedList(string[] args)
+            {
+                MyLinkedList myLinkedList = new MyLinkedList();
+                myLinkedList.AddAtHead(1);
+                myLinkedList.AddAtTail(3);
+                myLinkedList.AddAtIndex(1, 2);
+                myLinkedList.Get(1);
+                myLinkedList.DeleteAtIndex(1);
+                myLinkedList.Get(1);
+
+                Console.ReadKey();
+            }
+        }
+
+        private Node head;
+        int length = 0;
+
+        public static ListNode CreateSinglyLinkedList(int[] list) => CreateList(list, out _);
+
+        public static ListNode CreateSinglyLinkedList(int[] list, out ListNode tail) => CreateList(list, out tail);
+
+        private static ListNode CreateList(int[] list, out ListNode tail)
+        {
+            ListNode head = null;
+            tail = null;
+
+            if (list.Length > 0) tail = head = new ListNode(list[0]);
+
+            if (list.Length > 1)
+            {
+                for (var i = 1; i < list.Length; i++)
+                {
+                    tail = tail.next = new ListNode(list[i]);
+                }
+            }
+
+            return head;
+        }
+
+            public MyLinkedList()
+        {
+
+        }
+        public int Get(int index)
+        {
+            if (this.head == null)
+            {
+                return -1;
+            }
+
+            Node temp = this.head;
+            int curr = 0;
+
+            while (curr < index && temp != null)
+            {
+                temp = temp.next;
+                curr++;
+            }
+
+            if (curr == index && temp != null)
+            {
+                return temp.val;
+            }
+            else
+            {
+                return -1;
+            }
+        }
+
+        public void AddAtHead(int val)
+        {
+            Node newHead = new Node(val, this.head);
+            this.head = newHead;
+        }
+
+        public void AddAtTail(int val)
+        {
+            if (this.head == null)
+            {
+                this.head = new Node(val);
+                return;
+            }
+
+            Node temp = this.head;
+            while (temp.next != null)
+            {
+                temp = temp.next;
+            }
+
+            temp.next = new Node(val);
+        }
+
+        public void AddAtIndex(int index, int val)
+        {
+            if (index > length)
+            {
+                return;
+            }
+
+            if (index == 0)
+            {
+                AddAtHead(val);
+                return;
+            }
+
+            var newNode = new Node(val);
+            if (index == length)
+            {
+                AddAtTail(val);
+                return;
+            }
+
+            int countDown = 0;
+            var counter = head;
+            while (countDown != index && counter != null)
+            {
+                countDown++;
+                counter = counter.next;
+            }
+
+            if (counter == null)
+            {
+                return;
+            }
+
+            var prev = counter.prev;
+            newNode.prev = prev;
+            newNode.next = counter;
+            prev.next = newNode;
+            counter.prev = newNode;
+            length++;
+        }
+
+        public void DeleteAtIndex(int index)
+        {
+            if (index == 0 && head != null)
+            {
+                this.head = this.head.next;
+                return;
+            }
+
+            Node temp = this.head;
+            int curr = 0;
+
+            while (curr < index - 1 && temp != null)
+            {
+                temp = temp.next;
+                curr++;
+            }
+
+            if (curr == index - 1 && temp.next != null)
+            {
+                temp.next = temp.next.next;
+                return;
+            }
+        }
+
+
+        public class Node
+        {
+            public int val { get; set; }
+            public Node next { get; set; }
+            public Node prev { get; set; }
+
+
+            public Node(int v)
+            {
+                this.val = v;
+            }
+
+            public Node(int v, Node next)
+            {
+                this.val = v;
+                this.next = next;
+            }
+        }
+    }
+}
